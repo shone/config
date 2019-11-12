5f4a21337201899919951f6dd8002a4a0f70df5d
Josh Shone <josh.shone@gmail.com>
2019-11-13 00:30:04 +0100

    Add vlc to install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,1 +1,1 @@
-sudo pacman -S alacritty dolphin breeze-icons dolphin-plugins chromium kate git gwenview inkscape krita meld unzip dmenu grim okular
+sudo pacman -S alacritty dolphin breeze-icons dolphin-plugins chromium kate git gwenview inkscape krita meld unzip dmenu grim okular vlc
