1b812c36a114c3545130054347dcd3c2372b5c1a
Josh Shone <josh.shone@gmail.com>
2019-11-15 14:50:03 +0100

    Add waybar to sway config and install_packages.sh + formatting/comments

--- install_packages.sh
+++ install_packages.sh
@@ -1,1 +1,17 @@
-sudo pacman -S alacritty dolphin breeze-icons dolphin-plugins chromium kate git gwenview inkscape krita meld unzip dmenu grim okular vlc
+# Base packages
+sudo pacman -S \
+  alacritty                            \ # Terminal
+  kate                                 \ # Text editor
+  git meld                             \ # File comparison
+  dolphin dolphin-plugins breeze-icons \ # File browser + dependencies
+  dmenu                                \ # App launcher
+  chromium                             \
+  gwenview okular grim                 \ # Image viewing/capture
+  inkscape krita                       \ # Image editing
+  vlc                                  \
+  unzip
+
+# Extra for laptops
+sudo pacman -S \
+  waybar \
+  otf-font-awesome #(for special characters used by waybar)
