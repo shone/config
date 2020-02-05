44af6619e21c94adb57b99fb683b97b917058739
Josh Shone <josh.shone@gmail.com>
2020-02-05 12:38:11 +0100

    install_packages.sh: add more packages/comments

--- install_packages.sh
+++ install_packages.sh
@@ -1,18 +1,29 @@
 # Base packages
 sudo pacman -S \
-  alacritty                            \ # Terminal
-  kate                                 \ # Text editor
-  git meld                             \ # File comparison
-  dolphin dolphin-plugins breeze-icons \ # File browser + dependencies
-  dmenu                                \ # App launcher
-  pulseaudio                           \
-  chromium                             \
-  gwenview okular grim                 \ # Image viewing/capture
-  inkscape krita                       \ # Image editing
-  vlc                                  \
-  unzip
+  sway                    \ # Window manager (wayland)
+  alacritty               \ # Terminal (Super-C sway shortcut)
+  breeze-icons            \ # Icons for KDE apps (Dolphin, Kate)
+  kate                    \ # Text editor (Super-X sway shortcut)
+  dolphin dolphin-plugins \ # File browser (Super-E sway shortcut)
+  git                     \
+  meld                    \ # File comparison
+  unzip                   \
+  dmenu                   \ # App launcher ((Alt-D sway shortcut)
+  pulseaudio              \
+  chromium                \ (Super-W sway shortcut)
+  gwenview okular vlc     \ # Multimedia viewing
+  grim                    \ # Screen capture (Super-S sway shortcut)
+  inkscape krita            # Image editing
+
+yay -S \
+  wf-recorder \ # Video screen capture
+  spotify
 
 # Extra for laptops
 sudo pacman -S \
   waybar \
   otf-font-awesome #(for special characters used by waybar)
+yay -S simpfand-git
+systemctl enable simpfand
+systemctl start simpfand
+sensors # Check that fan control is working:
