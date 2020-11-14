f2658581e6827789ead0686811e38faf9c4ebc59
Josh Shone <josh.shone@gmail.com>
2020-11-14 18:45:47 +0100

    Add qt5-wayland to install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -3,6 +3,7 @@ BASE_PACKAGES=(
   alacritty               # Terminal (Super-C sway shortcut)
   wl-clipboard            # Wayland clipboard utilities
   waybar otf-font-awesome # System status indicators + special character font used by waybar
+  qt5-wayland             # For running KDE apps natively in wayland
   xorg-server-xwayland    # Required to run X applications like Chromium, Kate etc. on sway/wayland
   breeze-icons            # Icons for KDE apps (Dolphin, Kate)
   kate                    # Text editor (Super-X sway shortcut)
